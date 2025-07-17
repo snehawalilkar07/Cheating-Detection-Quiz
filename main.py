3dd0457993d0d846c7a76c90202cdecaa11a9900
Sneha Walilkar <comp58.snehawalilkar@gmail.com>
2025-07-17 23:53:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -449,4 +449,5 @@ def student_performance_summary(user_id):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
+
